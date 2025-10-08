ab8588ee4ff4fae020b7651e9891406372bf298c
Tuxa4Life <nikoloztuxa@gmail.com>
2025-10-09 00:01:20 +0400

    Update: little notice

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ const App = () => {
         {
             !mesh && <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', padding: '30px', borderRadius: '15px', boxShadow: '0 8px 20px rgba(0,0,0,0.3)', fontFamily: 'Arial, sans-serif' }}>
                 <h2 style={{ fontSize: '36px', margin: '0 0 15px 0', fontWeight: 'bold' }}>Welcome to OSM Map Builder</h2>
-                <p style={{ fontSize: '20px', margin: '0' }}>Please select a country and its city to generate a 3D model.</p>
+                <p style={{ fontSize: '20px', margin: '0 0 10px 0' }}>Please select a country and its city to generate a 3D model.</p>
+                <i style={{color: 'gray'}}>Some of the city data is not available or is too large to be rendered</i>
             </div>
 
         }
