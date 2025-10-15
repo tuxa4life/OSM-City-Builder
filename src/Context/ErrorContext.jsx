3161027ad484dac9df3b57b383d37f4b79a268b2
Tuxa4Life <nikoloztuxa@gmail.com>
2025-10-16 02:00:14 +0400

    fix: useCallback loops

--- src/Context/ErrorContext.jsx
+++ src/Context/ErrorContext.jsx
@@ -1,39 +1,53 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState, useCallback } from "react";
 
-const ErrorContext = createContext()
+const ErrorContext = createContext();
 const ErrorProvider = ({ children }) => {
-    const [errorMessage, setErrorMessage] = useState('')
-    const [error, setError] = useState(false)
-    const [errorAnimation, setErrorAnimation] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("");
+    const [error, setError] = useState(false);
+    const [errorAnimation, setErrorAnimation] = useState(false);
 
-    const [loaderMessage, setLoaderMessage] = useState('')
-    const [loaderState, setLoaderState] = useState(false)
+    const [loaderMessage, setLoaderMessage] = useState("");
+    const [loaderState, setLoaderState] = useState(false);
 
-    const clearError = () => {
-        setErrorAnimation(false)
-        setTimeout(() => setError(false), 500)
-    }
+    const clearError = useCallback(() => {
+        setErrorAnimation(false);
+        setTimeout(() => setError(false), 500);
+    }, []);
 
-    const showError = (text, duration = 3000) => {
-        setErrorMessage(text)
-        setError(true)
-        setErrorAnimation(true)
+    const showError = useCallback(
+        (text, duration = 3000) => {
+            setErrorMessage(text);
+            setError(true);
+            setErrorAnimation(true);
 
-        setTimeout(clearError, duration)
-    }
+            setTimeout(clearError, duration);
+        },
+        [clearError],
+    );
 
-    const data = { loaderState, loaderMessage, setLoaderMessage, setLoaderState, errorMessage, setErrorMessage, error, clearError, showError, errorAnimation }
-    return <ErrorContext.Provider value={data}>
-        {children}
-    </ErrorContext.Provider>
-}
+    const data = {
+        loaderState,
+        loaderMessage,
+        setLoaderMessage,
+        setLoaderState,
+        errorMessage,
+        setErrorMessage,
+        error,
+        clearError,
+        showError,
+        errorAnimation,
+    };
+    return (
+        <ErrorContext.Provider value={data}>{children}</ErrorContext.Provider>
+    );
+};
 
 export const useError = () => {
-    const context = useContext(ErrorContext)
+    const context = useContext(ErrorContext);
     if (!context) {
-        throw new Error('useContext must be used within a ErrorProvider')
+        throw new Error("useContext must be used within a ErrorProvider");
     }
-    return context
-}
-export { ErrorProvider }
-export default ErrorContext
+    return context;
+};
+export { ErrorProvider };
+export default ErrorContext;
